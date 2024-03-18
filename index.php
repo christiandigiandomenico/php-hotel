189bc6f5c4cec00352b015ef44641e7e64326ac8
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-03-18 16:35:37 +0100

    Milestone completata

--- index.php
+++ index.php
@@ -48,31 +48,54 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Hotels</title>
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     
-    <ul>
+<table class="table">
+  <thead>
+    <tr>
+        <?php
 
-    <?php
+            $keys = array_keys($hotels[0]);
 
-    foreach($hotels as $currentHotel) {
+            echo '<tr>';
+                foreach ($keys as $key) {
+            echo '<th scope="col">' . $key . '</th>';
+            }
+            echo '</tr>'; 
 
-       echo "<li> <ul>";
+        ?>
+    </tr>
+  </thead>
+  <tbody>
 
-       foreach($currentHotel as $key => $value) {
-            echo "
-            <li>
-                $key:  $value
-            </li>";
-       }
+        <?php
 
-       echo "</ul> </li>";
+        foreach($hotels as $currentHotel) {
+        echo '<tr>';
+        foreach ($currentHotel as $value) {
+        echo '<td>' . $value . '</td>';
+            }
+        echo '</tr>';
+        }
 
-    }
+        ?>
+    
+  </tbody>
+</table>
 
-    ?>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="true" id="flexCheckDefault" name="parkingcheck">
+  <label class="form-check-label" for="flexCheckDefault">
+    Parking
+  </label>
+</div>
+<input type="submit">
 
-    </ul>
+    <!--Bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
