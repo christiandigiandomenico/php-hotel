be4695339f73d0cb746cd79dc28c4a185c9b8c9b
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-03-18 18:32:47 +0100

    bonus uno completato

--- index.php
+++ index.php
@@ -40,6 +40,11 @@ $hotels = [
 
 ];
 
+
+$parkingHotelCheck = $_GET['parkingcheck'];
+
+$starHotelCheck = $_GET['starcheck'];
+
 ?>
 
 <!DOCTYPE html>
@@ -52,6 +57,10 @@ $hotels = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+
+<h1>Hotels</h1>
+
+<hr>
     
 <table class="table">
   <thead>
@@ -86,13 +95,72 @@ $hotels = [
   </tbody>
 </table>
 
-<div class="form-check">
+<?php
+
+$filteredHotels = [];
+
+foreach($hotels as $parkingHotel) {
+
+    if ($parkingHotel['parking'] == true) {
+        $filteredHotels[] = $parkingHotel;
+    }
+};
+
+?>
+
+<form>
+<div class="form-check mb-3">
   <input class="form-check-input" type="checkbox" value="true" id="flexCheckDefault" name="parkingcheck">
   <label class="form-check-label" for="flexCheckDefault">
     Parking
   </label>
 </div>
+
+<div class="form-floating">
+  <textarea class="form-control mb-3" placeholder="Leave a comment here" id="floatingTextarea" name="starcheck"></textarea>
+  <label for="floatingTextarea">Number of stars</label>
+</div>
+
 <input type="submit">
+</form>
+
+
+<table class="table">
+  <thead>
+    <tr>
+        <?php
+
+            $keysParking = array_keys($filteredHotels[0]);
+
+            echo '<tr>';
+                foreach ($keysParking as $key) {
+            echo '<th scope="col">' . $key . '</th>';
+            }
+            echo '</tr>'; 
+
+        ?>
+    </tr>
+  </thead>
+  <tbody>
+
+        <?php
+
+        if($parkingHotelCheck == true) {
+
+        foreach($filteredHotels as $currentHotelParking) {
+        echo '<tr>';
+        foreach ($currentHotelParking as $value) {
+        echo '<td>' . $value . '</td>';
+            }
+        echo '</tr>';
+        }
+
+    } 
+
+        ?>
+    
+  </tbody>
+</table>
 
     <!--Bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
